4a1b117a7fe604d705df360f414cb3b0cc329ed2
Erik Lisboa <bonitoerik@gmail.com>
2022-08-30 16:00:01 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp26/Nota.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ConsoleApp26
+{
+     class Nota
+    {
+       public string Nome;
+       public double Nota1;
+       public double Nota2;
+       public double Nota3;
+
+        public double SomaNotas()
+        {
+            return  Nota1 + Nota2 + Nota3;
+            
+        }
+        public double AppOrRep()
+        {
+            double media = Nota1 + Nota2 + Nota3;
+            if (media > 60)
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM " + (60.00 - media) + "PONTOS" );
+            }
+            return media;
+        }
+
+        public override string ToString()
+        {
+            return "NOTA FINAL = " + SomaNotas().ToString("F1", CultureInfo.InvariantCulture)
+             + "\n" + AppOrRep().ToString("F2",CultureInfo.InvariantCulture); 
+                
+                
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp26/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nota a = new Nota();
+
+
+            Console.WriteLine("Qual nome desse aluno?");
+            a.Nome= Console.ReadLine();
+            Console.WriteLine("Qual a nota desse aluno no Primeiro semestre:");
+            a.Nota1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual a nota desse aluno no Segundo semestre:");
+            a.Nota2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine("Qual a nota desse aluno no Terceiro semestre:");
+            a.Nota3 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            //Dados aluno
+            Console.WriteLine(a);
+        }
+    }
+}
